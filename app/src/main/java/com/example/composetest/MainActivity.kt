22a18a58a818910c5c9445b51a06cfee74c7829f
Alvin Dizon <alvin.dizon91@gmail.com>
2022-01-06 10:26:26 +0800

    Introduce columns - use extension function
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
 import com.example.composetest.ui.theme.Typography
 
@@ -19,9 +28,12 @@ class MainActivity : ComponentActivity() {
             ComposeTestTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Column {
-                        Greeting("Android")
-                        CustomText(text = "Alvin")
+                    Column(
+                        modifier = Modifier.height(500.dp).width(500.dp).background(Color.LightGray),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+                        CustomItem(weight = 1f)
                     }
                 }
             }
@@ -40,13 +52,26 @@ fun CustomText(text: String) {
 }
 
 
+@Composable
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
-        Column {
-            Greeting("Android")
-            CustomText(text = "Alvin")
+        Column(
+            modifier = Modifier.height(500.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
